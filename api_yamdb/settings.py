70077bf34f6b0b7a7985b94bcc657b589d9567d2
JohnnyBoomKek <summer1710@qq.com>
2020-10-17 15:58:58 +0800

    .

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': django.db.backends.postgresql,
+        'NAME': postgres,
+        'USER': postgres,
+        'PASSWORD': 88888888,
+        'HOST': db,
+        'PORT': 5432,
     }
 }
 
